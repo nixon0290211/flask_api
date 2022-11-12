accc4b78c9bdda01f606fea5d519d864e2a1da6e
chibatakashi <nixon0290211@icloud.com>
2022-11-12 10:24:39 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+import requests
+
+
+res = requests.get("https://zipcloud.ibsnet.co.jp/api/search?zipcode=7830060")
+# print(res.json()["status"])
+# print(res.json()["results"]["prefcode"])
+# print(res.text["status"])
+# print(len(res.json()["results"][0])
+＃長さを図る　len 
+# json = res.json()
+# result01 = json["results"][0]
+# print(res.json()["results"][0]["address"])
+# print(res.json()["results"][0]["address2"])
+# print(res.json()["results"][0]["address3"])
+# print(res.json()["results"][0]["prefcode"])
